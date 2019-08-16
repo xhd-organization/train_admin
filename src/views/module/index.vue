e7b11405f162503c16a9afa489fe11124df43d51
cnvp <183784945@qq.com>
2019-08-16 19:56:13 +0800

    模型管理

--- src/views/module/index.vue
+++ src/views/module/index.vue
@@ -4,20 +4,169 @@
       <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">
         创建模型
       </el-button>
+      <el-table
+        :key="tableKey"
+        v-loading="listLoading"
+        :data="list"
+        border
+        fit
+        highlight-current-row
+        style="width: 100%;"
+        @sort-change="sortChange"
+      >
+        <el-table-column label="模型id" prop="id" align="center" width="300">
+          <template slot-scope="scope">
+            <span>{{ scope.row.ids }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="模型名称" align="center">
+          <template slot-scope="scope">
+            <span>{{ scope.row.names }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" align="center" width="230" class-name="small-padding fixed-width">
+          <template slot-scope="{row}">
+            <el-button type="primary" size="mini" @click="handleUpdate(row)">
+              修改
+            </el-button>
+            <el-button v-if="row.status!='deleted'" size="mini" type="danger" @click="deleteBtn(row,'deleted')">
+              删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getModuleList" />
+      <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
+        <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="100px" style="width: 400px; margin-left:50px;">
+          <el-form-item label="Type" prop="type">
+            <el-select v-model="temp.type" class="filter-item" placeholder="Please select">
+              <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Date" prop="timestamp">
+            <el-date-picker v-model="temp.timestamp" type="datetime" placeholder="Please pick a date" />
+          </el-form-item>
+          <el-form-item label="模型名称" prop="names">
+            <el-input v-model="temp.names" />
+          </el-form-item>
+          <el-form-item label="Remark">
+            <el-input v-model="temp.remark" :autosize="{ minRows: 2, maxRows: 4}" type="textarea" placeholder="Please input" />
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">
+            Cancel
+          </el-button>
+          <el-button type="primary" @click="dialogStatus==='create'?createData():updateData()">
+            Confirm
+          </el-button>
+        </div>
+      </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
 import { fetchModuleList } from '@/api/module'
+import Pagination from '@/components/Pagination'
+
+const calendarTypeOptions = [
+  { key: 'CN', display_name: 'China' },
+  { key: 'US', display_name: 'USA' },
+  { key: 'JP', display_name: 'Japan' },
+  { key: 'EU', display_name: 'Eurozone' }
+]
+
+const calendarTypeKeyValue = calendarTypeOptions.reduce((acc, cur) => {
+  acc[cur.key] = cur.display_name
+  return acc
+}, {})
+
 export default {
+  name: 'Module',
+  components: { Pagination },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'info',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    },
+    typeFilter(type) {
+      return calendarTypeKeyValue[type]
+    }
+  },
+  data() {
+    return {
+      tableKey: 0,
+      list: [],
+      total: 0,
+      listLoading: true,
+      listQuery: {
+        page: 1,
+        limit: 20,
+        names: ''
+      },
+      showReviewer: false,
+      dialogFormVisible: false,
+      dialogStatus: '',
+      textMap: {
+        update: '编辑',
+        create: '创建'
+      },
+      temp: {
+        id: undefined,
+        remark: '',
+        timestamp: new Date(),
+        names: '',
+        type: '',
+        status: 'published'
+      },
+      calendarTypeOptions,
+      statusOptions: ['published', 'draft', 'deleted'],
+      rules: {
+        type: [{ required: true, message: 'type is required', trigger: 'change' }],
+        timestamp: [{ type: 'date', required: true, message: 'timestamp is required', trigger: 'change' }],
+        names: [{ required: true, message: 'title is required', trigger: 'blur' }]
+      }
+    }
+  },
   mounted() {
-    fetchModuleList()
+    this.getModuleList()
   },
   methods: {
+    getModuleList() {
+      fetchModuleList(this.listQuery).then(data => {
+        console.log(data.items)
+        this.list = data.items
+        this.total = data.total
+        this.listLoading = false
+      })
+    },
     // 添加模型
     handleCreate() {
       this.$router.push('/module/create')
+    },
+    sortChange(data) {
+      const { prop, order } = data
+      if (prop === 'id') {
+        this.sortByID(order)
+      }
+    },
+    handleUpdate(row) {
+      this.temp = Object.assign({}, row) // copy obj
+      this.temp.timestamp = new Date(this.temp.timestamp)
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    deleteBtn(row) {
+      console.log(row)
     }
   }
 }
