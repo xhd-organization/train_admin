d4159b0a09d5aa9a6e430e86d9ad36eff24328be
wuchen <380223773@qq.com>
2019-09-10 09:11:30 +0800

    登陆栏背景色

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -172,7 +172,7 @@ export default {
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg:#283443;
+$bg:#fff;
 $light_gray:#333;
 $cursor: #333;
 
